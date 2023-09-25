212af74f6369fa60121d923b52ceabb39656f18e
Mohammed Doulfakar <mohammed.doulfakar@gmail.com>
2023-09-25 15:32:57 +0100

    members components

---
+++ components/server/server-channel.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Channel, ChannelType, MembershipRole, Server } from "@prisma/client";
+import { Edit, Hash, Mic, Trash, Video, Lock } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { ActionTooltip } from "../action-tooltip";
+
+interface ServerChannelProps {
+  channel: Channel;
+  server: Server;
+  role: MembershipRole;
+}
+const iconMap = {
+  [ChannelType.TEXT]: Hash,
+  [ChannelType.VOICE]: Mic,
+  [ChannelType.VIDEO]: Video,
+};
+
+export const ServerChannel = ({
+  channel,
+  server,
+  role,
+}: ServerChannelProps) => {
+  const router = useRouter();
+  const params = useParams();
+  const ChannelIcon = iconMap[channel.type];
+  return (
+    <div
+      className={cn(
+        "group px-2 py-2 rounded-md flex items-center cursor-pointer gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
+        params?.channelId === channel.id && "bg-zinc-700/20 dark:bg-zinc-700"
+      )}
+    >
+      <ChannelIcon className="flex-shrink-0 text-zinc-500 dark:text-zinc-400 h-4 w-4" />
+      <p
+        className={cn(
+          "line-clamp-1 font-semibold text-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
+          params?.channelId === channel.id &&
+            "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+        )}
+      >
+        {channel.name}
+      </p>
+      {channel.name !== "general" && role !== MembershipRole.GUEST && (
+        <div className="ml-auto flex items-center gap-x-2">
+          <ActionTooltip label="Edit Channel">
+            <Edit className="hidden  group-hover:block w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:text-zinc-400 dark:hover:text-zinc-300 transition" />
+          </ActionTooltip>
+          <ActionTooltip label="Delete Channel">
+            <Trash className="hidden group-hover:block w-4 h-4 text-rose-500 hover:text-rose-600 dark:text-rose-400 dark:hover:text-rose-300 transition" />
+          </ActionTooltip>
+        </div>
+      )}
+
+      {channel.name === "general" && (
+        <Lock className="ml-auto w-4 h-4 text-zinc-500 dark:text-zinc-400" />
+      )}
+    </div>
+  );
+};
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -14,6 +14,10 @@ import {
   ShieldCheck,
   Video,
 } from "lucide-react";
+import { Separator } from "../ui/separator";
+import { ServerSection } from "./server-section";
+import { ServerChannel } from "./server-channel";
+import { ServerMember } from "./server-member";
 
 interface ServerSidebarProps {
   serverId: string;
@@ -133,6 +137,88 @@ const ServerSidebar = async ({ serverId }: ServerSidebarProps) => {
             ]}
           />
         </div>
+        <Separator className="bg-zinc-200 dark:bg-zinc-700 rounded-md my-2" />
+        {!!textChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              server={server}
+              sectionType="channels"
+              channelType={ChannelType.TEXT}
+              role={role}
+              label="Text Channels"
+            />
+            <div className="space-y-[2px]">
+              {textChannels.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  server={server}
+                  role={role}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {!!audioChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              server={server}
+              sectionType="channels"
+              channelType={ChannelType.VOICE}
+              role={role}
+              label="Audio Channels"
+            />
+            <div className="space-y-[2px]">
+              {audioChannels.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  server={server}
+                  role={role}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {!!videoChannels?.length && (
+          <div className="mb-2">
+            <ServerSection
+              server={server}
+              sectionType="channels"
+              channelType={ChannelType.VIDEO}
+              role={role}
+              label="Video Channels"
+            />
+            <div className="space-y-[2px]">
+              {videoChannels.map((channel) => (
+                <ServerChannel
+                  key={channel.id}
+                  channel={channel}
+                  server={server}
+                  role={role}
+                />
+              ))}
+            </div>
+          </div>
+        )}
+
+        {!!members?.length && (
+          <div className="mb-2">
+            <ServerSection
+              server={server}
+              sectionType="members"
+              role={role}
+              label="Members"
+            />
+            <div className="space-y-[2px]">
+              {members.map((member) => (
+                <ServerMember key={member.id} member={member} server={server} />
+              ))}
+            </div>
+          </div>
+        )}
       </ScrollArea>
     </div>
   );
